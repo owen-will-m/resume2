d578d9e396f0c2518613c4f3c5c7ae6728df14b3
owen-will-m <wallowin66@gmail.com>
2023-01-17 10:27:44 -0800

    made projects a two column layout

--- src/Projects.js
+++ src/Projects.js
@@ -5,15 +5,21 @@ export default function Projects(props) {
   return (
       <div className="Projects">
 <h1>projects</h1>
+<div className='wrappingColumnContainer'>
+<div className="wrappingColumn">
 <Subheader title="🎨 Design"/>
 <div className="clickableLink" onClick={()=>props.setModal('ri')}>Rhode Island Unemployment Redesign</div>
 <div className="clickableLink" onClick={()=>props.setModal('hmp')}>Healthcare Management Portal</div>
+</div>
+<div className="wrappingColumn">
 <Subheader title="🔢 Development"/>
 <div className="clickableLink" onClick={()=>props.setModal('pjs')}>Processing JS Artwork</div>
 <div className="clickableLink" onClick={()=>props.setModal('lc')}>Lewis & Clark Expedition Maps</div>
-
+</div>
+</div>
 </div>
 
   );
 }
+
 
